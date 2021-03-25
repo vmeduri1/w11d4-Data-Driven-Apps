bf8139e1ab134d44084f390580ecf6e23add5224
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-25 11:47:25 -0500

    Added config files

--- app.js
+++ app.js
@@ -40,6 +40,17 @@ app.use((err, req, res, next) => {
     }
 });
 
+// Generic error handler.
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    const isProduction = process.env.NODE_ENV === 'production';
+    res.render('error', {
+        title: 'Server Error',
+        message: isProduction ? null : err.message,
+        stack: isProduction ? null: err.stack,
+    });
+});
+
 // Define a port and start listening for connections.
 
 module.exports = app;
