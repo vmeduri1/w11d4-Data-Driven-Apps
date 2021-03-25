de9e87929c6e455beedc8e06c71990e3e774545d
Vishaal Meduri <vishaal.meduri@gmail.com>
2021-03-25 17:21:58 -0500

    Home Stretch

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 // ./routes.js
 const express = require('express');
 const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
 
 const db = require('./db/models');
 
@@ -10,6 +11,33 @@ const csrfProtection = csrf({ cookie: true });
 
 const asyncHandler = (handler) =>  (req, res, next) => handler(req, res, next).catch(next);
 
+const bookValidators = [
+    check('title')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Titlee')
+        .isLength({ max: 255 })
+        .withMessage('Title must not be more than 255 characters long'),
+    check('author')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Author')
+        .isLength({ max: 100 })
+        .withMessage('Author must not be more than 100 characters long'),
+    check('releaseDate')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Release Date')
+        .isISO8601()
+        .withMessage('Please provide a valid date for Release Date'),
+    check('pageCount')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Page Count')
+        .isInt({ min: 0 })
+        .withMessage('Please provide a valid integer for Page Count'),
+    check('publisher')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Publisher')
+        .isLength({ max: 100 })
+        .withMessage('Publisher must not be more than 100 characters long'),
+];
 
 router.get('/', asyncHandler(async (req, res) => {
     const books = await db.Book.findAll({ order: [['title', 'ASC']] });
@@ -25,39 +53,39 @@ router.get('/book/add', csrfProtection, (req, res) => {
     });
 });
 
-router.post('/book/add', csrfProtection, asyncHandler(async (req, res, next) => {
-    const {
-        title,
-        author,
-        releaseDate,
-        pageCount,
-        publisher,
-    } = req.body;
+router.post('/book/add', csrfProtection, bookValidators,
+    asyncHandler(async (req, res, next) => {
+        const {
+            title,
+            author,
+            releaseDate,
+            pageCount,
+            publisher,
+        } = req.body;
 
-    const book = db.Book.build({
-        title,
-        author,
-        releaseDate,
-        pageCount,
-        publisher,
-    });
+        const book = db.Book.build({
+            title,
+            author,
+            releaseDate,
+            pageCount,
+            publisher,
+        });
 
-    try {
-        await book.save();
-        res.redirect('/');
-    } catch (err) {
-        if (err.name === 'SequelizeValidationError') {
-            const errors = err.errors.map((error) => error.message);
+        const validatorErrors = validationResult(req);
+
+        if (validatorErrors.isEmpty()) {
+            await book.save();
+            res.redirect('/');
+        } else {
+            const errors = validatorErrors.array().map((error) => error.msg);
             res.render('book-add', {
-              title: 'Add Book',
-              book,
-              errors,
-              csrfToken: req.csrfToken(),
+                title: 'Add Book',
+                book,
+                errors,
+                csrfToken: req.csrfToken(),
             });
-          } else {
-            next(err);
-          }
-    }
-}));
+        }
+    })
+);
 
 module.exports = router;
